3285423f26ac53f301e2cea9b152f2746adf112f
semhoutman <s.houtman1@students.uu.nl>
2023-04-09 14:52:59 +0200

    profile changes

--- public/script/profile.js
+++ public/script/profile.js
@@ -1,3 +1,6 @@
+const profileDetailsElement = document.getElementById("profileDetails")
+const userId = parseInt(localStorage.getItem("userId"));
+
 async function getUserOrderHistory() {
   let userId = parseInt(localStorage.getItem("userId"));
   let response = await fetch(`/get-user-order-history/${userId}`);
@@ -9,6 +12,42 @@ async function getUserOrderHistory() {
 
     document.getElementsByTagName(body).appendChild(orderContainer);
   });
+}
+
+async function renderProfileDetails() {
+  let user = await fetch(`/get-user/${userId}`)
+  let dataList = await user.json()
+  let data = dataList[0]
+  if (data.fullName) {
+    let username = data.fullName;
+    let email = data.email;
+    let creditCard = data.creditCard;
+    let address = data.address;
+  
+    let usernameElement = document.createElement('p');
+    usernameElement.innerText = `Hello, ${username}!`
+
+    let emailElement = document.createElement('p');
+    emailElement.innerText = `Email: ${email}!`
+  
+    let creditCardElement = document.createElement('p');
+    creditCardElement.innerText = `Creditcard: ${creditCard}`;
+  
+    let addressElement = document.createElement('p');
+    addressElement.innerText = `Address: ${address}`;
+  
+    profileDetailsElement.appendChild(usernameElement);
+    profileDetailsElement.appendChild(emailElement);
+    profileDetailsElement.appendChild(creditCardElement);
+    profileDetailsElement.appendChild(addressElement);
+  } else {
+    const loginLink = document.createElement('a');
+    loginLink.innerText = 'Login';
+    loginLink.setAttribute('href', '/login');
+
+    profileDetailsElement.appendChild(loginLink)
+  }
+  
 }
 
 function loginSucces() {
