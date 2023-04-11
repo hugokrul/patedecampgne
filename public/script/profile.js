7877653127606376e573cf1bdd9814aa26a86ff2
semhoutman <s.houtman1@students.uu.nl>
2023-04-11 10:22:44 +0200

    changes

--- public/script/profile.js
+++ public/script/profile.js
@@ -3,8 +3,12 @@ const userId = parseInt(localStorage.getItem("userId"));
 
 async function getUserOrderHistory() {
   let userId = parseInt(localStorage.getItem("userId"));
-  let response = await fetch(`/get-user-order-history/${userId}`);
+  let response = await fetch(`/group5/get-user-order-history/${userId}`, {
+    method: "POST",
+  });
   let dataArray = await response.json();
+
+  console.log(dataArray);
 
   dataArray.map((indOrder, index) => {
     const orderContainer = document.createElement("div");
