3ef7db381bb7370e121d47391782469228cce983
hugokrul <hugo.krul@gmail.com>
2023-04-16 18:08:57 +0200

    fuzzy search

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,8 @@
+const movieList = document.getElementById("movieList")
+
 let now = [];
-var soon = [];
+let soon = [];
+let movieTitles = [];
 const slidingContentElement = document.getElementById("slidingcontent");
 const slidingContentElement2 = document.getElementById("slidingcontent2");
 
@@ -20,6 +23,7 @@ async function getMovies() {
   let response = await fetch("/get-all-movies");
   let data = await response.json();
   for (let i = 0; i < data.length; i++) {
+    movieTitles.push(data[i])
     let dates = data[i].playingSpan.split("_");
     if (checkCurrentDateBetweenDates(dates[0], dates[1])) {
       now.push([
@@ -108,4 +112,63 @@ function showNow() {
     aElement.appendChild(containerElement);
     slidingContentElement2.appendChild(aElement);
   }
+}
+
+function search() {
+  movieList.innerHTML = "";
+
+  let input = document.getElementById("search").value;
+
+  const options = {
+    includeScore: true,
+    keys: ["title"]
+  };
+  
+  const fuse = new Fuse(movieTitles, options);
+  const result = fuse.search(input);
+  let movies = [];
+  result.forEach((movie, index) => {
+    if (movie.score < 0.5) {
+      movies.push(movie.item);
+    }
+  });
+
+  movies.map((indMovie, index) => {
+    const movieItem = document.createElement("div");
+    movieItem.className = "movieItem";
+    const movieItemFlex = document.createElement("div");
+    movieItemFlex.className = "flex movieItemFlex";
+    movieItem.appendChild(movieItemFlex);
+
+    const movieImageContainer = document.createElement("div");
+    movieImageContainer.className = "movieImageContainer";
+    const movieImage = document.createElement("img");
+    movieImage.className = "movieImage";
+    const imageElementsrc = `images/playing/${indMovie.title
+      .replace(/ /g, "-")
+      .replace(/:/g, "")
+      .replace(".", "")}.jpg`;
+    movieImage.src = imageElementsrc;
+    movieImageContainer.appendChild(movieImage);
+    movieItemFlex.appendChild(movieImageContainer);
+
+    const movieItemTextContent = document.createElement("div");
+    movieItemTextContent.className = "movieItemTextContent";
+    const movieItemTitle = document.createElement("h3");
+    movieItemTitle.className = "movieItemTitle";
+    movieItemTitle.innerHTML = indMovie.title;
+
+    const movieItemDescription = document.createElement("p");
+    movieItemDescription.className = "movieItemDescription";
+    movieItemDescription.innerHTML = indMovie.plot;
+
+    movieItemTextContent.appendChild(movieItemTitle);
+    movieItemTextContent.appendChild(movieItemDescription);
+
+    movieItemFlex.appendChild(movieItemTextContent);
+
+    console.log(movieItem);
+    movieList.appendChild(movieItem);
+  });
+
 }
